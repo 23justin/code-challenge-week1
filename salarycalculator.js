23bb2b6118f33a6e7911db9f637a66a78bba081b
14justin <justinmwangi40@gmail.com>
2024-03-26 15:59:25 +0300

    resubmit

--- salarycalculator.js
+++ salarycalculator.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-const basicSalary = parseFloat(prompt("Enter basic   Salary: "));
+const basicSalary = parseFloat(prompt("Enter basic Salary: "));
 
 function kratax(basicSalary, benefits = 0) {
     let nssf = 0;
@@ -12,7 +12,37 @@ function kratax(basicSalary, benefits = 0) {
             nhifDeduction = 150;
         } else if (basicSalary >= 6000 && basicSalary <= 7999) {
             nhifDeduction = 300;
-        } // add more conditions...
+        } else if (basicSalary >= 8000 && basicSalary <= 11999) {
+            nhifDeduction = 400;
+        } else if (basicSalary >= 12000 && basicSalary <= 14999) {
+            nhifDeduction = 500;
+        } else if (basicSalary >= 15000 && basicSalary <= 19999) {
+            nhifDeduction = 600;
+        } else if (basicSalary >= 20000 && basicSalary <= 24999) {
+            nhifDeduction = 750;
+        } else if (basicSalary >= 25000 && basicSalary <= 29999) {
+            nhifDeduction = 850;
+        } else if (basicSalary >= 30000 && basicSalary <= 34999) {
+            nhifDeduction = 900;
+        } else if (basicSalary >= 35000 && basicSalary <= 39999) {
+            nhifDeduction = 950;
+        } else if (basicSalary >= 40000 && basicSalary <= 44999) {
+            nhifDeduction = 1000;
+        } else if (basicSalary >= 45000 && basicSalary <= 49999) {
+            nhifDeduction = 1100;
+        } else if (basicSalary >= 50000 && basicSalary <= 59999) {
+            nhifDeduction = 1200;
+        } else if (basicSalary >= 60000 && basicSalary <= 69999) {
+            nhifDeduction = 1300;
+        } else if (basicSalary >= 70000 && basicSalary <= 79999) {
+            nhifDeduction = 1400;
+        } else if (basicSalary >= 80000 && basicSalary <= 89999) {
+            nhifDeduction = 1500;
+        } else if (basicSalary >= 90000 && basicSalary <= 99999) {
+            nhifDeduction = 1600;
+        } else if (basicSalary >= 100000) {
+            nhifDeduction = 1700;
+        }
         return nhifDeduction;
     }
 
@@ -23,7 +53,13 @@ function kratax(basicSalary, benefits = 0) {
         let personalRelief = 2400;
         if (payeableIncome > 0 && payeableIncome <= 24000) {
             grossTax = payeableIncome * 0.10;
-        } // add more conditions...
+        } else if (payeableIncome > 24000 && payeableIncome <= 52000) {
+            grossTax = 2400 + (payeableIncome - 24000) * 0.25;
+        } else if (payeableIncome > 52000 && payeableIncome <= 102000) {
+            grossTax = 10000 + (payeableIncome - 52000) * 0.30;
+        } else if (payeableIncome > 102000) {
+            grossTax = 26000 + (payeableIncome - 102000) * 0.35;
+        }
         paye = Math.floor(grossTax - personalRelief);
         let netsalary = payeableIncome - paye;
         return { paye, netsalary };
